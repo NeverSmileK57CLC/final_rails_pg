f1faebcc5deddcdba6cf604d048a6c70ce3e5307
NeverSmileK57CLC <neversmile12b1@gmail.com>
2015-04-10 15:03:21 +0700

    Create final rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FinalRailsPg::Application.config.secret_key_base = 'adc5b9ccd69fab5b007fd78cf82af59c0ff5ab98ed82a3e06d74dc1c5a22391acae98d1e7dd28c1d59a313b1a2fadbda1811c2e78a67e09fb5f912aec706e2ef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FinalRailsPg::Application.config.session_store :cookie_store, key: '_final_rails_pg_session'
